3e51144f7c8269f4de6d7580fa5b21fc4152fc01
Will Saulnier <wsaulnier1004@gmail.com>
2024-06-24 21:58:57 -0400

    working on visuals

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,39 @@
 </script>
 
 <template>
-  <p class="text-lg">Hello world</p>
+  <!-- Top section -->
+  <div class="pt-[50px] pb-[20px] px-[16px] flex space-x-[10px]">
+    <!-- Search bar -->
+    <div class="bg-[#D9D9D9] rounded-lg border border-gray-300 flex-auto py-[5px] px-[8px] flex space-x-[5px]">
+      <unicon name="search" />
+      <input type="text" 
+        placeholder="Search" 
+        class="placeholder:text-black placeholder:font-bold w-full bg-transparent">
+    </div>
+    
+    <!-- Filter Button -->
+    <button><unicon name="filter" /></button>
+  </div>
+
+  <!-- bottom section -->
+  <div class="bg-[#E4F4FF] h-screen">
+    <br/>
+    <div class="bg-white m-[10px]">
+      <p>Mow Lawn</p>
+      <p>$100</p>
+      <p>15 minutes</p>
+    </div>
+    <div class="bg-white m-[10px]">
+      <p>Mow Lawn</p>
+      <p>$100</p>
+      <p>15 minutes</p>
+    </div>
+    <div class="bg-white m-[10px]">
+      <p>Mow Lawn</p>
+      <p>$100</p>
+      <p>15 minutes</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
